d2b0b7cd81ee192946082439ff0f8e77e23103bc
Mac Home <machome@gmail.com>
2017-02-18 23:13:17 +0000

    Initial Tip Calculator - followed video walkthrough

--- tippy/ViewController.swift
+++ tippy/ViewController.swift
@@ -9,17 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+  
+  
+  @IBOutlet weak var tipLabel: UILabel!
+  @IBOutlet weak var totalLabel: UILabel!
+  @IBOutlet weak var billField: UITextField!
+  @IBOutlet weak var tipControl: UISegmentedControl!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  @IBAction func onTap(_ sender: AnyObject) {
+    view.endEditing(true)
+  }
+  
+  @IBAction func calculateTip(_ sender: AnyObject) {
+    
+    let tipPercetages = [0.18, 0.2, 0.25]
+    
+    let bill = Double(billField.text!) ?? 0
+    let tip = bill * tipPercetages[tipControl.selectedSegmentIndex]
+    let total = bill + tip
+    
+    tipLabel.text = String(format: "$%.2f", tip)
+    totalLabel.text = String(format: "$%.2f", total)
+    
+  }
+  
 }
 
