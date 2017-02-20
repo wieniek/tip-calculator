3dc06e60d3e9f9375d51e2bb140e7df75ec42b91
Mac Home <machome@gmail.com>
2017-02-20 22:52:02 +0000

    Added DataStore structure to implement persistent storage

---
+++ tippy/DataStore.swift
@@ -0,0 +1,40 @@
+//
+//  DataStore.swift
+//  tippy
+//
+//  Created by Home Mac on 20/02/2017.
+//  Copyright © 2017 Home Mac. All rights reserved.
+//
+
+import Foundation
+
+// DataStore provides persisten storage of default tip amounts to user defaults
+struct DataStore {
+  
+  var tipPercentages = [18, 20, 25] // predifined tip amounts for first app run
+  let tipPercentagesKey = "tippy_tip_percentages01" // key value for user defaults database
+  
+  var defaultPercentageIndex = 0 // predefined for first app run
+  let defaultPercentageIndexKey = "tippy_default_percentage_index01" // key value for user defaults database
+  
+  // converts provided parameters to text and saves them to user defaults
+  func saveSettings(tipPercentages percentages: [Int], defaultPercentageIndex index: Int) {
+    let defaults = UserDefaults.standard
+    defaults.set(percentages.map { String($0) }, forKey: tipPercentagesKey)
+    defaults.set(String(index), forKey: defaultPercentageIndexKey)
+    defaults.synchronize()
+  }
+  
+  // loads values from user defaults and converts them to numbers
+  mutating func loadSettings() {
+    let defaults = UserDefaults.standard
+    if let array = defaults.stringArray(forKey: tipPercentagesKey) {
+      print("Array from user defaults = \(array)")
+      tipPercentages = array.map { Int($0)! }
+    }
+    if let value = defaults.string(forKey: defaultPercentageIndexKey) {
+      print("String from user defaults = \(value)")
+      defaultPercentageIndex = Int(value)!
+    }
+  }
+}
