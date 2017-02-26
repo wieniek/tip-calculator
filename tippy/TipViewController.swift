9dca47ec5963793287bff75386f1401bfcab86eb
Mac Home <machome@gmail.com>
2017-02-26 18:20:21 +0000

    Added simple view animation

--- tippy/TipViewController.swift
+++ tippy/TipViewController.swift
@@ -15,12 +15,45 @@ class TipViewController: UIViewController {
   @IBOutlet weak var billField: UITextField!
   @IBOutlet weak var tipControl: UISegmentedControl!
   
+  
+  @IBOutlet weak var tipResultsView: UIView!
+  
+  
+  
   // Set focus to the view and dismiss keyboard when tapped
   @IBAction func onTap(_ sender: AnyObject) {
     view.becomeFirstResponder()
     view.endEditing(true)
   }
   
+  
+  @IBAction func animateTipResultsView(_ sender: UITextField) {
+    
+    if let numberOfCharacters = billField.text?.characters.count {
+      
+      let tipResultsViewCenterY = Double(tipResultsView.center.y)
+      let offset = view.bounds.height
+      
+      print("TEXT LENGHT = \(numberOfCharacters)")
+      print("VIEW CENTER = \(tipResultsViewCenterY)")
+      
+      if numberOfCharacters == 1 && tipResultsViewCenterY > 300 {
+        
+        animateView(moveByPoints: -offset)
+      } else if numberOfCharacters == 0 && tipResultsViewCenterY < 300 {
+        animateView(moveByPoints: offset)
+      }
+    }
+  }
+  
+  func animateView(moveByPoints points: CGFloat) {
+    UIView.animate(withDuration: 0.5, delay: 0.0, options: .curveEaseOut, animations: {
+
+        self.tipResultsView.center.y += points
+
+    }, completion: { finished in print("End of amimation1")})
+  }
+
   // Recalculate tip amount and total using provided values
   @IBAction func calculateTip(_ sender: AnyObject) {
     
@@ -53,6 +86,9 @@ class TipViewController: UIViewController {
                                            selector: #selector(saveBillFieldTextInput),
                                            name: .UIApplicationWillResignActive,
                                            object: nil)
+    
+    //Animation part
+    tipResultsView.center.y += view.bounds.height
     
     //Setup UISegmentedControl properties
     tipControl.loadTitles()
@@ -97,5 +133,23 @@ class TipViewController: UIViewController {
                                               name: .UIApplicationWillResignActive,
                                               object: nil)
   }
+  
+  
+  override func viewDidAppear(_ animated: Bool) {
+    
+    UIView.animate(withDuration: 3.0, delay: 2.0, options: .curveEaseOut, animations: {
+      
+      //Animation part
+      //self.tipResultsView.center.y -= self.view.bounds.height
+      
+      
+      //self.tipResultsView.transform = CGAffineTransform(translationX: -20, y: +50)
+      //self.billField.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+    }, completion: { finished in print("End of amimation1")})
+    
+    //    UIView.animate(withDuration: 1.0, delay: 3.0, options: .curveEaseOut, animations: {
+    //      self.billField.transform = CGAffineTransform(translationX: +20, y: -50)
+    //      //self.billField.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+    //    }, completion: { finished in print("End of amimation")})
+  }
 }
-
